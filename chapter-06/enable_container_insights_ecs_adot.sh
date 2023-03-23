bb4478857c5fc6d6834a6cb9faf199c5000b76d3
Fabio de Oliveira <fabio.braga@gmail.com>
2023-03-23 13:11:44 +0100

    Create default VPC if it doesn't exist

--- chapter-06/enable_container_insights_ecs_adot.sh
+++ chapter-06/enable_container_insights_ecs_adot.sh
@@ -155,6 +155,14 @@ fi
 
 echo "Using ECS task definition with ARN: $ECS_TASK_DEFINITION_ARN"
 
+DEFAULT_VPC_ID=$(aws ec2 describe-vpcs \
+    --filters Name=is-default,Values=true \
+    --query "Vpcs[*].VpcId" --output text)
+    
+if [ -z ${DEFAULT_VPC_ID+x} ]; then
+    aws ec2 create-default-vpc
+fi
+
 if ! $(aws ecs list-services --cluster o11y-on-aws --query "serviceArns[*]" --output text | grep -w -q o11y-on-aws-firelens); then
     echo "ECS service not found, creating..."
     DEFAULT_VPC_ID=$(aws ec2 describe-vpcs \
