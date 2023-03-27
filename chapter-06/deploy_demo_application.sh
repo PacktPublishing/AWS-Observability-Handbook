fac79da934049186e1473c3cc856089fcb7853f7
Fabio de Oliveira <fabio.braga@gmail.com>
2023-03-27 22:35:07 +0200

    Change to a copilot-cli implementation

--- chapter-06/deploy_demo_application.sh
+++ chapter-06/deploy_demo_application.sh
@@ -9,22 +9,22 @@ git clone https://github.com/aws-containers/ecsdemo-frontend
 git clone https://github.com/aws-containers/ecsdemo-nodejs 
 git clone https://github.com/aws-containers/ecsdemo-crystal 
 
-# Execute the commands below to deploy the required infrastructure: 
-
-cd ~/environment/ecsdemo-platform/cdk 
-pip install -r requirements.txt 
-cdk context --clear && cdk deploy --require-approval never 
 
 # Execute the commands below to deploy the sample application microservices: 
 
-cd ~/environment/ecsdemo-frontend/cdk 
-pip install -r requirements.txt 
-cdk context --clear && cdk deploy --require-approval never 
+cd ~/environment/ecsdemo-frontend
+copilot init --app ecsworkshop --type "Load Balanced Web Service" --name ecsdemo-frontend --dockerfile "./Dockerfile" --deploy
 
-cd ~/environment/ecsdemo-nodejs/cdk 
-pip install -r requirements.txt 
-cdk context --clear && cdk deploy --require-approval never 
+cat << EOF >> copilot/ecsdemo-frontend/manifest.yml
+variables:
+  CRYSTAL_URL: "http://ecsdemo-crystal.test.ecsworkshop.local:3000/crystal"
+  NODEJS_URL: "http://ecsdemo-nodejs.test.ecsworkshop.local:3000"
+EOF
+copilot deploy --app ecsworkshop --name ecsdemo-frontend
 
-cd ~/environment/ecsdemo-crystal/cdk 
-pip install -r requirements.txt 
-cdk context --clear && cdk deploy --require-approval never
+cd ~/environment/ecsdemo-nodejs
+copilot init --app ecsworkshop --type "Backend Service" --name ecsdemo-nodejs --dockerfile "./Dockerfile" --deploy 
+
+cd ~/environment/ecsdemo-crystal
+git rev-parse --short=7 HEAD > code_hash.txt
+copilot init --app ecsworkshop --type "Backend Service" --name ecsdemo-crystal --dockerfile "./Dockerfile" --deploy 
