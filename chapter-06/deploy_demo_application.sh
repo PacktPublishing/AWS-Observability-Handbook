e51c2e42aac835f922429ab7bb934928efff610b
Fabio de Oliveira <fabio.braga@gmail.com>
2023-03-28 10:26:17 +0200

    Change app/cluster name

--- chapter-06/deploy_demo_application.sh
+++ chapter-06/deploy_demo_application.sh
@@ -13,18 +13,18 @@ git clone https://github.com/aws-containers/ecsdemo-crystal
 # Execute the commands below to deploy the sample application microservices: 
 
 cd ~/environment/ecsdemo-frontend
-copilot init --app ecsworkshop --type "Load Balanced Web Service" --name ecsdemo-frontend --dockerfile "./Dockerfile" --deploy
+copilot init --app o11y-on-aws --type "Load Balanced Web Service" --name ecsdemo-frontend --dockerfile "./Dockerfile" --deploy 
 
 cat << EOF >> copilot/ecsdemo-frontend/manifest.yml
 variables:
   CRYSTAL_URL: "http://ecsdemo-crystal.test.ecsworkshop.local:3000/crystal"
   NODEJS_URL: "http://ecsdemo-nodejs.test.ecsworkshop.local:3000"
 EOF
-copilot deploy --app ecsworkshop --name ecsdemo-frontend
+copilot deploy --app o11y-on-aws --name ecsdemo-frontend
 
 cd ~/environment/ecsdemo-nodejs
-copilot init --app ecsworkshop --type "Backend Service" --name ecsdemo-nodejs --dockerfile "./Dockerfile" --deploy 
+copilot init --app o11y-on-aws --type "Backend Service" --name ecsdemo-nodejs --dockerfile "./Dockerfile" --deploy 
 
 cd ~/environment/ecsdemo-crystal
 git rev-parse --short=7 HEAD > code_hash.txt
-copilot init --app ecsworkshop --type "Backend Service" --name ecsdemo-crystal --dockerfile "./Dockerfile" --deploy 
+copilot init --app o11y-on-aws --type "Backend Service" --name ecsdemo-crystal --dockerfile "./Dockerfile" --deploy 
