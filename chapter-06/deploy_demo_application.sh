7c299518f1c5a57a78b7b64f09793e4830b8c2ef
Fabio de Oliveira <fabio.braga@gmail.com>
2023-03-28 10:54:34 +0200

    Remove project, not used

--- chapter-06/deploy_demo_application.sh
+++ chapter-06/deploy_demo_application.sh
@@ -4,7 +4,6 @@
 
 cd ~/environment 
 
-git clone https://github.com/aws-containers/ecsdemo-platform 
 git clone https://github.com/aws-containers/ecsdemo-frontend 
 git clone https://github.com/aws-containers/ecsdemo-nodejs 
 git clone https://github.com/aws-containers/ecsdemo-crystal 
